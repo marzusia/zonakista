fad63709129036ea96b713b53edea473068ea254
Marzena <marzena.budek@outlook.com>
2021-09-19 15:08:32 +0100

    move media_url to environment

--- zonakista/settings.py
+++ zonakista/settings.py
@@ -131,13 +131,13 @@ STATIC_URL = env('STATIC_URL')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'zonakista', 'static')
 
+# Media root for uploaded files
+
+MEDIA_URL = env('MEDIA_URL')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Media root for uploaded files
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
