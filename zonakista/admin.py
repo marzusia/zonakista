6ab51ea0cb6799f0a4ac6da8f842fea48ac9330f
Marzena <marzena.budek@outlook.com>
2021-09-22 14:25:32 +0100

    make admin site registering nicer

--- zonakista/admin.py
+++ zonakista/admin.py
@@ -5,38 +5,15 @@ from .models.articles import Article
 from .models.organisation import Organisation
 from .models.lexicon import Word, Sense, Example, Gloss, Derivation
 
-@admin.register(Citizen)
-class CitizenAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(City)
-class CityAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Organisation)
-class OrganisationAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Word)
-class WordAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Sense)
-class SenseAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Example)
-class ExampleAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Gloss)
-class GlossAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Derivation)
-class DerivationAdmin(admin.ModelAdmin):
-    pass
+for model in [
+    Citizen,
+    City,
+    Article,
+    Organisation,
+    Word,
+    Sense,
+    Example,
+    Gloss,
+    Derivation
+]:
+    admin.site.register(model)
